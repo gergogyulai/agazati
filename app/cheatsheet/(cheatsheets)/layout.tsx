d251cbf2dde0a21776f605a8ca8f6013f7170d6d
gergogyulai <gergoo.gyulai@icloud.com>
2024-06-12 10:25:50 +0200

    Update cheatsheet page layout for mobile responsibility

--- app/cheatsheet/(cheatsheets)/layout.tsx
+++ app/cheatsheet/(cheatsheets)/layout.tsx
@@ -1,7 +1,7 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex justify-center p-8">
-      <article className="container prose max-w-screen-md p-4 text-justify dark:prose-invert">
+    <div className="flex justify-center pt-8">
+      <article className="container prose max-w-screen-md p-4 dark:prose-invert">
         {children}
       </article>
     </div>
